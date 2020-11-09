b7e00cc890f8f03cc58526def180b5860e84546c
Alex Price <alex@alexprice.co>
2020-11-09 11:25:53 -0600

    Add time record command

---
+++ open_records.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Add a time record
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.icon 🧭
+
+osascript -e 'tell application "Google Chrome" to open location "https://airtable.com/tbll9KI55BZs3xxFq/viwZYUxym7W5aRYUN?blocks=hide"'
+echo "Opened records page"
